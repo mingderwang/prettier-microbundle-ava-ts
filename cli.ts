2f1539bab739031811f39b0ddd1ab1e7777bc23e
Ming-der Wang <mingderwang@gmail.com>
2023-07-26 09:54:06 +0800

    v1.1.5

--- cli.ts
+++ cli.ts
@@ -1,3 +1,4 @@
 #!/usr/bin/env node
-import { add } from './src/main';
-console.log(`Hello, this is your CLI! 1 + 1 = ${add(1,1)} ` );
+import { add, greet } from './src/main';
+console.log(greet('ming'));
+console.log(`This is your CLI! 1 + 1 = ${add(1,1)} ` );
