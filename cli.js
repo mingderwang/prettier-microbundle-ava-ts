a72ffb5a1966c043a29cdd168042ef193c7baade
Ming-der Wang <mingderwang@gmail.com>
2023-07-26 09:23:58 +0800

    remove js files

--- cli.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const main_1 = require("./src/main");
-console.log(`Hello, this is your CLI! 1 + 1 = ${(0, main_1.add)(1, 1)} `);
--- test/add.test.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-const ava_1 = require('@fast-check/ava');
-const main_1 = require('../src/main');
-// Define the property-based test
-(0, ava_1.testProp)('Adding two numbers is commutative', [ava_1.fc.integer(), ava_1.fc.integer()], (t, a, b) => {
-  t.is((0, main_1.add)(a, b), (0, main_1.add)(b, a));
-});
